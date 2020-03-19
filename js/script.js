05f7a6d74af5496482822c55e109b97ac5401b01
FrankLaptop <fasvi6@gmail.com>
2020-03-18 21:26:54 -0400

    modified class photos page to include a modal that appears whenever an image is clicked, to show a full size version;

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
     $('.carousel').carousel( {interval: 2000} );
+
+    $('.classPhotoImg').click(function(e){
+        let title = e.target.nextElementSibling.innerText
+        let image = e.target.src;
+        $('#classPhotoModalTitle').text(title);
+        $('#classPhotoModalImage').attr('src', image);
+        $('#classPhotoModalAnchor').attr('href', image);
+        $('#classPhotoModal').modal('show');
+    });
+    $('#classPhotoModal').on('hidden.bs.modal', function() {
+        $(this).removeData('bs.modal');
+    });
 })
